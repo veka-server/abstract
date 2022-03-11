16a4633addf5054e3334cb15756647317ace6b5d
Veka <veka610@gmail.com>
2022-03-11 22:17:14 +0100

    Update PluginAbstract.php

--- src/PluginAbstract.php
+++ src/PluginAbstract.php
@@ -13,24 +13,24 @@ abstract class PluginAbstract implements PluginInterface
     protected abstract static function getVIEW():array ;
 
     public static function getPathView():array {
-        $r = self::getVIEW();
-        foreach (self::getAllRequiredPlugin() as $plugin){
+        $r = static::getVIEW();
+        foreach (static::getAllRequiredPlugin() as $plugin){
             return array_merge($plugin::getPathView(), $r);
         }
         return $r;
     }
 
     public static function getPathJS():array {
-        $r = self::getJS();
-        foreach (self::getAllRequiredPlugin() as $plugin){
+        $r = static::getJS();
+        foreach (static::getAllRequiredPlugin() as $plugin){
             return array_merge($plugin::getPathJS(), $r);
         }
         return $r;
     }
 
     public static function getPathCSS():array {
-        $r = self::getCSS();
-        foreach (self::getAllRequiredPlugin() as $plugin){
+        $r = static::getCSS();
+        foreach (static::getAllRequiredPlugin() as $plugin){
             return array_merge($plugin::getPathCSS(), $r);
         }
         return $r;
