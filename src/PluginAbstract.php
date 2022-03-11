2d293c09fc5464297f8b956e41df4b731071c501
veka-server <veka610@gmail.com>
2022-03-11 21:59:56 +0100

    init

---
+++ src/PluginAbstract.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace VekaServer\TableForm;
+
+use VekaServer\Interfaces\PluginInterface;
+
+abstract class PluginAbstract implements PluginInterface
+{
+
+    protected abstract static function getAllRequiredPlugin():array ;
+    protected abstract static function getCSS():array ;
+    protected abstract static function getJS():array ;
+    protected abstract static function getVIEW():array ;
+
+    public static function getPathView():array {
+        $r = self::getVIEW();
+        foreach (self::getAllRequiredPlugin() as $plugin){
+            return array_merge($plugin::getPathView(), $r);
+        }
+        return $r;
+    }
+
+    public static function getPathJS():array {
+        $r = self::getJS();
+        foreach (self::getAllRequiredPlugin() as $plugin){
+            return array_merge($plugin::getPathJS(), $r);
+        }
+        return $r;
+    }
+
+    public static function getPathCSS():array {
+        $r = self::getCSS();
+        foreach (self::getAllRequiredPlugin() as $plugin){
+            return array_merge($plugin::getPathCSS(), $r);
+        }
+        return $r;
+    }
+
+}
